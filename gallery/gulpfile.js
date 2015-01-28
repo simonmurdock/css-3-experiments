70ccf2898b737c813e1a51e958f1b2220e9e5c81
simon murdock <thesimo@gmail.com>
2015-01-28 21:00:02 +0000

    not slimming anymore

--- gallery/gulpfile.js
+++ gallery/gulpfile.js
@@ -7,22 +7,18 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
 
-// // Concatenate & Minify JS
-// gulp.task('scripts', function() {
-//     return gulp.src('js/linear_partition.js')
-//         .pipe(gulp.dest('wwwroot/js'))
-//         .pipe(rename('linear_partition.min.js'))
-//         .pipe(uglify({mangle:true, compress:true}))
-//         .pipe(gulp.dest('wwwroot/js'));
-// });
-// Concatenate & Minify JS
+
+gulp.task('css', function() {
+    return gulp.src('css/css.css')
+        .pipe(minifyCSS({keepBreaks:false}))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
 gulp.task('scripts', function() {
     return gulp.src('js/gallery.js')
-        .pipe(gulp.dest('wwwroot/js'))
-        .pipe(rename('gallery.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('wwwroot/js'));
 });
 
 // Default Task
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'css']);
